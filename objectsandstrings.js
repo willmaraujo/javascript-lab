cf83f157bd539823691e1935c5632cd06622e878
Williams Araujo <williams.araujo@actuasys.com>
2020-07-20 14:38:00 +0100

    More samples

---
+++ objectsandstrings.js
@@ -0,0 +1,24 @@
+var string1 = 'Tampa';
+var string2 = string1;
+string1 = 'Venice';
+ 
+console.log(string2);
+ 
+ 
+////////////////////////////////
+ 
+ 
+var person1 = {
+  name: 'Alex',
+  age: 30
+};
+ 
+var person2 = person1;
+ 
+person2.name = 'Kyle';
+ 
+console.log(person1);
+
+// var person2 = Object.assign({}, person1);
+// person2.name = 'Will';
+// console.log(person1);
---
+++ types.js
@@ -0,0 +1,6 @@
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof {});
+console.log(typeof []);
+console.log(Array.isArray([]));
+console.log([] instanceof Array);
