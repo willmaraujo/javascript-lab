bda3c18685059193d17066f72a18e2f854f82110
Williams Araujo <williams.araujo@actuasys.com>
2020-07-20 12:26:42 +0100

    More samples

---
+++ invoked.js
@@ -0,0 +1,18 @@
+function getTotal() {
+    var args = Array.prototype.slice.call(arguments);
+    
+    if (args.length === 2) {
+      return args[0] + args[1];
+    }
+    else if (args.length === 1) {
+      return function(num2) {
+        console.log(`args0 ${args[0]} and num2 ${num2}`);
+        return args[0] + num2;
+      };
+    }
+  }
+   
+  //console.log(getTotal(10, 20));
+//   console.log(getTotal(5, 40));
+   console.log(getTotal(3)(30));
+//   console.log(getTotal(8)(12));
---
+++ list.js
@@ -0,0 +1,8 @@
+const list1 = [1, 2, 3, 4, 5];
+const list2 = list1.slice();
+// const list2 = list1.concat([]);
+ 
+list1.push(6, 7, 8);
+ 
+console.log('List 1: ', list1);
+console.log('List 2: ', list2);
